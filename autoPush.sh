4eae4164c055a41e5e7e321c12faf023668c291a
Alonza0314 <a0984078318@gmail.com>
2024-05-18 18:13:41 +0800

    update via git

--- autoPush.sh
+++ autoPush.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+bg_red='\033[41m'
+bg_green='\033[42m'
+reset_color='\033[0m'
+
+echo -e "${bg_red}start adding...${reset_color}"
+
 git add .
 
-echo "add complete"
+echo -e "${bg_green}add complete${reset_color}"
+
+echo -e "${bg_red}start committing...${reset_color}"
 
 git commit -m "update via git"
 
-echo "commit complete"
+echo -e "${bg_green}commit complete${reset_color}"
+
+echo -e "${bg_red}start pushing...${reset_color}"
 
 git push
 
-echo "push complete"
+echo -e "${bg_green}push complete${reset_color}"
