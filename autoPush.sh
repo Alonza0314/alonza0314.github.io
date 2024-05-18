027d7e5b50701edd199443ed36ca91ed69a4a69f
Alonza0314 <a0984078318@gmail.com>
2024-05-18 18:17:01 +0800

    update via git

--- autoPush.sh
+++ autoPush.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-bg_red='\033[41m'
+bg_blue='\033[44m'
 bg_green='\033[42m'
 reset_color='\033[0m'
 
-echo -e "${bg_red}start adding...${reset_color}"
+echo -e "${bg_blue}start adding...${reset_color}"
 
 git add .
 
 echo -e "${bg_green}add complete${reset_color}"
 
-echo -e "${bg_red}start committing...${reset_color}"
+echo -e "${bg_blue}start committing...${reset_color}"
 
 git commit -m "update via git"
 
 echo -e "${bg_green}commit complete${reset_color}"
 
-echo -e "${bg_red}start pushing...${reset_color}"
+echo -e "${bg_blue}start pushing...${reset_color}"
 
 git push
 
