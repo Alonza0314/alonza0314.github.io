a7781fddff1d3cd9b54ad406a6bc6794e644a32b
Alonza0314 <a0984078318@gmail.com>
2024-05-22 01:06:51 +0800

    update via git

--- js/index.js
+++ js/index.js
@@ -1,9 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var languageSwitcher = document.getElementById('languageSwitcher');
+    var languageSwitcherZH = document.getElementById('languageSwitcherZH');
+    var languageSwitcherEN = document.getElementById('languageSwitcherEN');
     
-    languageSwitcher.addEventListener('click', function(event) {
+    languageSwitcherZH.addEventListener('click', function(event) {
         event.preventDefault();
         topbarLanguage();
         bottombarLanguage();
+        indexLanguage();
+    });
+    
+    languageSwitcherEN.addEventListener('click', function(event) {
+        event.preventDefault();
+        topbarLanguage();
+        bottombarLanguage();
+        indexLanguage();
     });
 });
+
+function indexLanguage() {
+    var rc = document.getElementById("right-content");
+    var rcen = document.getElementById("right-content-en");
+    
+    var about = document.getElementById("about")
+    var abouten = document.getElementById("about-en")
+    
+    if (rc.style.display === "none") {
+        rc.style.display = "block";
+        rcen.style.display = "none";
+    } else {
+        rc.style.display = "none";
+        rcen.style.display = "block";
+    }
+
+    if (about.style.display === "none") {
+        about.style.display = "flex";
+        abouten.style.display = "none";
+    } else {
+        about.style.display = "none";
+        abouten.style.display = "flex";
+    }
+}
+
