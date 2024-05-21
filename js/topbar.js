87fe09935d3ad518907356f7e6eaead8f1fa81bd
Alonza0314 <a0984078318@gmail.com>
2024-05-21 23:29:45 +0800

    update via git

---
+++ js/topbar.js
@@ -0,0 +1,12 @@
+// function toggleLanguage() {
+//     var chineselink = document.getElementById("link");
+//     var englishlink = document.getElementById("link-en");
+    
+//     if (chineselink.style.display === "none") {
+//         chineselink.style.display = "unset";
+//         englishlink.style.display = "none";
+//     } else {
+//         chineselink.style.display = "none";
+//         englishlink.style.display = "unset";
+//     }
+// }
