af3bab800556ff22252f045f29dcc758f861f08d
Alonza0314 <a0984078318@gmail.com>
2024-05-23 05:28:14 +0800

    update via git

---
+++ js/about.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var languageSwitcherZH = document.getElementById('languageSwitcherZH');
+    var languageSwitcherEN = document.getElementById('languageSwitcherEN');
+    
+    languageSwitcherZH.addEventListener('click', function(event) {
+        event.preventDefault();
+        topbarLanguage();
+        bottombarLanguage();
+        Language();
+    });
+    
+    languageSwitcherEN.addEventListener('click', function(event) {
+        event.preventDefault();
+        topbarLanguage();
+        bottombarLanguage();
+        Language();
+    });
+});
+
+function Language() {
+    var hpl = document.getElementById("head-part-left");
+    var hplen = document.getElementById("head-part-left-en");
+
+    
+    var bp = document.getElementById("body-part");
+    var bpen = document.getElementById("body-part-en");
+
+    var c = document.getElementById("current");
+    var cen = document.getElementById("current-en");
+    
+    if (hpl.style.display === "none") {
+        hpl.style.display = "flex";
+        hplen.style.display = "none";
+    } else {
+        hpl.style.display = "none";
+        hplen.style.display = "flex";
+    }
+    
+    if (bp.style.display === "none") {
+        bp.style.display = "flex";
+        bpen.style.display = "none";
+    } else {
+        bp.style.display = "none";
+        bpen.style.display = "flex";
+    }
+
+    if (c.style.display === "none") {
+        c.style.display = "flex";
+        cen.style.display = "none";
+    } else {
+        c.style.display = "none";
+        cen.style.display = "flex";
+    }
+}
+
